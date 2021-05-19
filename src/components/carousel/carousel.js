c0491acfa8433ab2326bb8591af0f0428534783c
Duma Doniagara Sambora <duma@dusdusan.com>
2021-05-19 09:59:45 +0700

    carrousel

---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Redirect, NavLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+export class Carousel extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+
+      }
+   }
+
+   componentDidMount(){
+
+   }
+
+   render(){
+      return(
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+            <div className="carousel-item active">
+                <img src="https://cf.shopee.co.id/file/8059ccea76ec8403e202a5bc5e1eaf45_tn" className="d-block w-100" alt="..." 
+                style={{width: 'auto',
+                        minHeight: 450,
+                        maxHeight: 500,
+                        height: '100%',
+                        background: '#ccc',
+                        objectFit:'contain'
+                        }}
+                />
+                <div class="carousel-caption d-none d-md-block" style={{color:'#7c7d7c'}}>
+                    <h5>Riverdale - South Side Serpents</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div className="carousel-item">
+                <img src="https://cf.shopee.co.id/file/a362df6a8832f6d8b05c690fd6ed51a8_tn" className="d-block w-100" alt="..." style= {{width: 'auto',
+                        minHeight: 450,
+                        maxHeight: 500,
+                        height: '100%',
+                        background: '#ccc',
+                        objectFit:'contain'
+                        }}/>
+                        <div class="carousel-caption d-none d-md-block" style={{color:'#7c7d7c'}}>
+                    <h5>The Big Bang Theory - Engineering Oompa Loompas of Science</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div className="carousel-item">
+                <img src=" https://cf.shopee.co.id/file/9af1c0de7611b53ac23584e5a32b7523_tn" className="d-block w-100" alt="..." 
+                style= {{width: '100%',
+                minHeight: 450,
+                maxHeight: 500,
+                height: '100%',
+                background: '#ccc',
+                objectFit:'contain'
+                }}
+                />
+                <div class="carousel-caption d-none d-md-block" style={{color:'#7c7d7c'}}>
+                    <h5>The Big Bang Theory -  The Flash</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+            </button>
+      </div>
+      )
+   }
+
+}
+
+export default Carousel;
--- src/components/navigation/toolbar/toolbar.js
+++ src/components/navigation/toolbar/toolbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Redirect, NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import mmLogo from '../../../assets/images/mm-logo.jpg';
+import { ShoppingCartOutlined, UserOutlined, SearchOutlined } from '@ant-design/icons'; 
+import {Input} from 'antd';
 import './style.css';
 
 export class Toolbar extends Component {
@@ -18,17 +20,49 @@ export class Toolbar extends Component {
 
    render(){
       return(
-         <div className="toolbar">
-            <div className="logo-wrapper">
-               <img src={mmLogo} className="toolbar-logo"/>
-            </div>
-            <div className="toolbar-menu">
-               <div className="toolbar-submenu">Home</div>
-               <div className="toolbar-submenu">Category</div>
-               <div className="toolbar-submenu">Multiverse Market</div>
-               <div className="toolbar-submenu">Payment Confirmation</div>
-            </div>
+   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+         <img src={mmLogo} style={{width: 45, borderRadius:20}}/>
+         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+         </button>
+         <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{marginLeft:100}}>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+               <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item dropdown">
+               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Category
+               </a>
+               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">T-Shirt</a></li>
+                  <li><a className="dropdown-item" href="#">Hoodie</a></li>
+                  <li><a className="dropdown-item" href="#">Jacket</a></li>
+               </ul>
+            </li>
+            <li className="nav-item">
+               <a className="nav-link active" aria-current="page" href="#">Payment Confirmation</a>
+            </li>
+            <li className="nav-item sale">
+               <a className="nav-link active" aria-current="page" href="#">Sale!</a>
+            </li>
+            </ul>
+            <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search products..." aria-label="Search"/>
+            <button className="btn" type="submit" style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+               <SearchOutlined />
+            </button>
+            <button className="btn" style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+               <ShoppingCartOutlined style={{fontSize: 20}}/>
+            </button>
+            <button className="btn" style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+               <UserOutlined style={{fontSize: 20}}/>
+            </button>
+            </form>
          </div>
+      </div>
+   </nav>
       )
    }
 
